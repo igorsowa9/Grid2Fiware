725306eb3a93fcecf4db4a549932aee4c00e3671
igorsowa9 <igorsowa9@gmail.com>
2019-06-12 11:52:18 +0000

    pmu updated to 6 ch

--- cloud/pmu_provisioning.py
+++ cloud/pmu_provisioning.py
@@ -8,10 +8,10 @@ import sys
 n_pmu_analog_channels = 8  # determined by number of chanells in DAQ
 n_pmu_streams_to_cloud = 40  # 8x5 from each channel with have magn, ang, freq, rocof, time, ... (?)
 
-channel_names = np.array(["ch1", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8"])
+channel_names = np.array(["ch1", "ch2", "ch3", "ch4", "ch5", "ch6"])
 sub_names = np.array(["a", "b", "c", "d", "e"])
 
-channel_signals = np.array(["v1a", "v3a", "v5a", "v6a", "v7a", "v8a", "v9a", "v10a"])
+channel_signals = np.array(["v1a", "v3a", "v5a", "v8a", "v9a", "v10a"])
 sub_signals = np.array(["magnitude", "frequency", "angle", "rocof", "timestamp"])
 
 fiware_service = "grid_uc"
