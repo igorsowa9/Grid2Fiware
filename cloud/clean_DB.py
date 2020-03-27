77cc2d0a39da6671cafe9e9a36f5cec430999b15
IS <igorsowa9@gmail.com>
2020-03-27 11:00:24 +0000

    query for psycop2

--- cloud/clean_DB.py
+++ cloud/clean_DB.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import psycopg2
+import time
 
 dbname = "mtgrid_uc"
 
@@ -25,4 +26,18 @@ def clean():
     cursor.execute("DELETE FROM mtgrid_uc.etpmu; DELETE FROM mtgrid_uc.etrtds;");
 
 
-clean()
+def test_access():
+
+    conn = psycopg2.connect("dbname='" + dbname + "' user='' host='10.12.0.10' password=''")
+    cursor = conn.cursor()
+    cursor.execute("SELECT ts_measurement FROM mtgrid_uc.etrtds1 ORDER BY ts_measurement DESC LIMIT 1;")
+    r = cursor.fetchall()
+    print(r)
+    time.sleep(0.05)
+
+if __name__ == '__main__':
+    while True:
+        test_access()
+
+# clean()
+
